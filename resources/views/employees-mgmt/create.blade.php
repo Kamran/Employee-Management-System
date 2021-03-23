53f51785549f499906ea8bbeed6c658aaac7a26e
Kamran <75747459+Kamran@users.noreply.github.com>
2021-03-23 18:08:17 +0600

    Update create.blade.php

--- resources/views/employees-mgmt/create.blade.php
+++ resources/views/employees-mgmt/create.blade.php
@@ -128,7 +128,7 @@
                             <div class="col-md-4">
                                 {{-- Gender<input type="radio" name="gender" required > --}}
                                    <br> Gender  
-                                    <input type="radio" name="gender" value="male" > Male  
+                                    <input type="radio" name="gender" value="male"  checked="checked"> Male  
                                     <input type="radio" name="gender" value="female">Female   
                                     <input type="radio" name="gender" value="other">other  
                                     
